2267aa1a7a4c32132e085ddaf4d3688b10e45eed
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 02:14:09 +0900

    feat: 제품 컨트롤러 상태코드 변경

--- src/products/controller.ts
+++ src/products/controller.ts
@@ -1,21 +1,26 @@
 import service from "./service"
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
 export default {
   getAll: async (req: Request, res: Response) => {
     try {
       const data = await service.getAll()
-      return res.json({ data: data, message: "success" })
+      return res.status(StatusCodes.OK).json({ data: data, message: "success" })
     } catch (error: any) {
-      return res.json({ message: error.message });
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        error: "Internal Server Error",
+      });
     }
   },
   post: async (req: Request, res: Response) => {
     try {
       service.post(req.body)
-      return res.json({ message: "success" })
+      return res.status(StatusCodes.CREATED).json({ message: "success" })
     } catch (error: any) {
-      return res.json({ message: error.message });
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        error: "Internal Server Error",
+      });
     }
   }
 }
