2ad29673b09127378a520921862380f821afdc58
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 01:32:26 +0900

    feat: 쇼핑몰 제품 POST 요청 저장소 추가

--- src/products/repository.ts
+++ src/products/repository.ts
@@ -1,7 +1,23 @@
-import repository from "./repository";
+import { Products } from '../products/models/productModel';
+import { Details } from '../products/models/detailModel';
+import { DetailImages } from '../products/models/detailImageModel';
+import { PackageQuantitys } from '../products/models/packageQueatityModel';
+import { States } from '../products/models/stateModel';
+import connection from '../db';
 
 export default {
   getAll: () => {
     return "hello"
+  },
+  post: (body: any) => {
+    connection.transaction(async (transaction) => {
+      await Products.create(body, { raw: true, transaction });
+      await Details.create(body, { raw: true, transaction });
+      await DetailImages.create(body, { raw: true, transaction });
+      await PackageQuantitys.create(body, { raw: true, transaction });
+      await States.create(body, { raw: true, transaction });
+    }).catch((err) => {
+      throw err;
+    })
   }
 }
