adb3438df964f4d6208714c83045c27710a99c1b
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 01:35:12 +0900

    feat: 쇼핑몰 제품 POST 요청 컨트롤러 추가

--- src/products/controller.ts
+++ src/products/controller.ts
@@ -5,5 +5,9 @@ export default {
   getAll: (req: Request, res: Response) => {
     const data = service.getAll()
     return res.json({ data: data, message: "success" })
+  },
+  post: (req: Request, res: Response) => {
+    service.post(req.body)
+    return res.json({ message: "success" })
   }
 }
