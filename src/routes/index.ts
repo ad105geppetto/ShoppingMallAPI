fcd350a9c4fd90e303a6e0ea0eb05409b5ed6fe8
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 04:52:20 +0900

    feat: 회원가입 POST 요청 라우터 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import productsRouter from "./productsRouter"
+import usersRouter from "./usersRouter"
 
 const router = express.Router();
 
 router.use("/products", productsRouter);
+router.use("/signup", usersRouter);
 
 export default router;
