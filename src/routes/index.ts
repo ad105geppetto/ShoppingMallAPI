e8e9c98425665de2e23842d48dd1e0967b58f068
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 08:40:19 +0900

    feat: 주문 등록 POST 라우터 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import productsRouter from "./productsRouter"
 import signupRouter from "./signupRouter"
 import loginRouter from "./loginRouter"
 import dropoutRouter from "./dropoutRouter"
+import orderRouter from "./orderRouter"
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.use("/products", productsRouter);
 router.use("/signup", signupRouter);
 router.use("/login", loginRouter);
 router.use("/dropout", dropoutRouter);
+router.use("/orders", orderRouter);
 
 export default router;
