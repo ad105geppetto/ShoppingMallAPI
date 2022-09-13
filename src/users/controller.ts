e0090ce865aca978339d2cd87e6509b1cd5bbfde
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 06:09:43 +0900

    feat: 로그인 POST 요청 컨트롤러 추가

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -13,4 +13,14 @@ export default {
       });
     }
   },
+  login: async (req: Request, res: Response) => {
+    try {
+      const data = await service.login(req.body)
+      return res.status(StatusCodes.OK).json({ data: data, message: "success" })
+    } catch (error: any) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        error: "Internal Server Error",
+      });
+    }
+  },
 }
