aa8b442aea8b6f78cc7b32e48b745345c3e78060
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 06:15:21 +0900

    feat: 로그인 POST 요청 라우터 수정

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import productsRouter from "./productsRouter"
 import signupRouter from "./signupRouter"
-import login from "./loginRouter"
+import loginRouter from "./loginRouter"
 
 const router = express.Router();
 
 router.use("/products", productsRouter);
 router.use("/signup", signupRouter);
-router.use("/login", login);
+router.use("/login", loginRouter);
 
 export default router;
