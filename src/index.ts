54beeb3196b8737d29b46f132b236b998df36655
ad105geppetto <ad105geppetto@gmail.com>
2022-09-13 13:59:36 +0900

    feat: 쇼핑몰 제품 리스트 GET 요청 라우터 추가

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express'
+import indexRouter from "./routes";
 const app = express()
 const port = 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
