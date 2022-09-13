49b1344647ba930fc44e9715827185e7366d6523
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 06:08:20 +0900

    feat: 로그인 POST 요청 저장소 추가

--- src/users/respository.ts
+++ src/users/respository.ts
@@ -1,9 +1,28 @@
 import { Users } from "./models/usersModel"
 import module from "./modules/hashPassword"
 
+interface user {
+  password: string,
+  salt: string
+}
+
 export default {
   signup: async (body: any) => {
     const hashInfo = module.hashPassword(body.password)
     await Users.create({ ...body, ...{ password: hashInfo.hash, salt: hashInfo.salt } });
+  },
+  login: async (body: any) => {
+    const user = await Users.findOne({ where: { email: body.email } });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    const newPassword = module.makePasswordHashed(user.password, user.salt)
+
+    if (newPassword !== user.password) {
+      throw new Error();
+    }
+    return user.id
   },
 }
