7b9bd35eb410c23b64f0251a4df7212cd0e0ce03
ad105geppetto <ad105geppetto@gmail.com>
2022-09-14 16:18:22 +0900

    feat: 데이터베이스 sample 수정

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import connection from './db';
 import indexRouter from "./routes";
 import { Grades } from './users/models/gradeModel';
+import { Users } from './users/models/usersModel';
+import { Products } from './products/models/productModel';
 const app = express()
 const port = 3000
 
@@ -14,6 +16,17 @@ connection.sync({ force: true }).then(async () => {
   console.log("Database synced successfully")
   await Grades.create({ type: "관리자" })
   await Grades.create({ type: "이용자" })
+  await Users.create({
+    email: "wooyongwoo@naver.com",
+    password: "sha123",
+    salt: "fdfdfdf",
+    grade_id: 1,
+    username: "우영우"
+  })
+  await Products.create({
+    title: "싱싱한 자두 1kg/3kg",
+    thumbnail_image_path: "http://shopping-mall.com",
+  })
 }).catch((err: Error) => {
   console.log("Error", err)
 })
